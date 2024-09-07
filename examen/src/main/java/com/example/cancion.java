daedb260a6c4f3cdc9efeafa4b47e3baebae09de
Camilo9807 <camilo.galvis98@gmail.com>
2024-09-07 15:27:09 -0500

    cambi1

---
+++ examen/src/main/java/com/example/cancion.java
@@ -0,0 +1,86 @@
+package com.example;
+
+public class cancion {
+    private String titulo;
+    private String artista;
+    private int duracion; // en segundos
+    private String genero;
+    private String album;
+    
+    private static final int SEGUNDOS_POR_MINUTO = 60;
+
+    //contructor por defecto
+    public cancion() {
+        this.titulo = "Desconocido";
+        this.artista = "Desconocido";
+        this.duracion = 0;
+        this.genero = "Desconocido";
+        this.album = "Desconocido";
+    }
+    
+    // Constructor con parámetros
+    public cancion(String titulo, String artista, int duracion) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.duracion = duracion;
+        this.genero = "Desconocido"; // Inicializado por defecto
+        this.album = "Desconocido";  // Inicializado por defecto
+    }
+
+    // Metodo Getters y setters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    private int calcularDuracionMinutos() {
+        return duracion / SEGUNDOS_POR_MINUTO;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Artista: " + artista);
+        System.out.println("Duración: " + calcularDuracionMinutos() + " minutos");
+        System.out.println("Género: " + genero);
+        System.out.println("Álbum: " + album);
+    }
+    public void mostrarInformacion(String usuario) {
+        mostrarInformacion();
+        System.out.println("Escuchado por: " + usuario);
+    }
+}
